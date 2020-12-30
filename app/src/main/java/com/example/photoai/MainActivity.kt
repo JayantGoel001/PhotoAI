ea386a381d09b438144ab1bb930a85d12bced973
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-30 21:51:41 +0530

    Initial Commit

--- app/src/main/java/com/example/photoai/MainActivity.kt
+++ app/src/main/java/com/example/photoai/MainActivity.kt
@@ -1,31 +1,39 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.photoai
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import com.example.photoai.databinding.ActivityMainBinding
 import com.google.android.material.chip.Chip
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
-import com.google.mlkit.vision.common.InputImage
+import com.google.firebase.ml.vision.text.FirebaseVisionText
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private var isText = false
+    private lateinit var imageView: ImageView
+    private lateinit var switch:SwitchCompat
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val switch = findViewById<SwitchCompat>(R.id.switch1)
+        switch = findViewById(R.id.switch1)
+        imageView = findViewById(R.id.imageView)
         switch.setOnClickListener{
            isText = switch.isChecked
         }
         binding.fab.setOnClickListener {
             pickImage()
         }
-
     }
 
     private fun processImageTagging(bitmap: Bitmap){
@@ -33,19 +41,43 @@ class MainActivity : AppCompatActivity() {
         FirebaseVision.getInstance().cloudImageLabeler.processImage(visionImage).addOnSuccessListener { tags ->
             binding.chipGroup.removeAllViews()
             tags.sortByDescending { it.confidence }
-            for (it in tags){
-                Chip(this,null,R.style.Widget_MaterialComponents_Chip_Choice).apply {
-                    text = this.text
-                    binding.chipGroup.addView(this)
-                }
+            tags.map {
+                Chip(this,null,R.style.Widget_MaterialComponents_Chip_Choice).apply { text = it.text }
+            }.forEach {
+                binding.chipGroup.addView(it)
             }
         }.addOnFailureListener{
             Toast.makeText(this,it.message.toString(),Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun getImageFromData(data:Intent):Bitmap{
+    private fun getImageFromData(data:Intent?):Bitmap{
+        val selectedImage = data?.data
+        return MediaStore.Images.Media.getBitmap(this.contentResolver, selectedImage)
+    }
 
+    @SuppressLint("SetTextI18n")
+    private fun startTextRecognizing(bitmap: Bitmap){
+        if (binding.imageView.drawable!=null){
+            val image = FirebaseVisionImage.fromBitmap(bitmap)
+            val detector = FirebaseVision.getInstance().cloudTextRecognizer
+            detector.processImage(image).addOnSuccessListener {
+                processTextBlock(it!!)
+            }.addOnFailureListener {
+                binding.textView.text = "Failed"
+            }
+        }else{
+            Toast.makeText(this,"Failed",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun processTextBlock(result: FirebaseVisionText) {
+        binding.chipGroup.removeAllViews()
+        result.textBlocks.map {
+            Chip(this,null,R.style.Widget_MaterialComponents_Chip_Choice).apply { text = it.text }
+        }.forEach {
+            binding.chipGroup.addView(it)
+        }
     }
 
     private fun pickImage() {
@@ -57,5 +89,21 @@ class MainActivity : AppCompatActivity() {
     }
     companion object{
         private var IMAGE_PICK_CODE = 108
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode==Activity.RESULT_OK && requestCode== IMAGE_PICK_CODE){
+            val bitmap = getImageFromData(data)
+            bitmap.apply {
+                imageView.setImageBitmap(this)
+                if (!isText){
+                    processImageTagging(bitmap)
+                }else{
+                    startTextRecognizing(bitmap)
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+
     }
 }
